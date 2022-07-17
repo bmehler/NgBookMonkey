c96d1d416a3aeb5a74276cd52b2b44beadf6ac97
Bernhard Mehler <bernhardmehler@bernhards-imac.localdomain>
2022-07-17 14:53:38 +0200

    Interation 1 property-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'bm-root',
   template: '<bm-book-list></bm-book-list>',
-  styles: []
 })
 export class AppComponent {
-  title = 'BookMonkey';
 }
---
+++ src/app/book-list-item/book-list-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'bm-book-list-item',
+  templateUrl: './book-list-item.component.html',
+  styles: [
+  ]
+})
+export class BookListItemComponent {
+
+ @Input() book: Book;
+
+}
