03dece87304394518143c8fac60fbcd9a9a680a4
Bernhard Mehler <bernhardmehler@bernhards-imac.localdomain>
2022-07-18 09:42:07 +0200

    Interation 2 Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookListItemComponent } from './book-list-item/book-list-item.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { BookStoreService } from './shared/book-store.service';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { BookDetailsComponent } from './book-details/book-details.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    BookStoreService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
